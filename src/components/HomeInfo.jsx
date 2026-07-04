c12c1a5ae48859c80b1b8ae8f48d9b8c40a8d46d
jeeltarpada84-create <jeeltarpada84@gmail.com>
2026-07-04 20:35:49 +0530

    Update HomeInfo.jsx

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -14,7 +14,7 @@ const InfoBox = ({ text, link, btnText }) => (
 const renderContent = {
   1: (
     <h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">
-      Hi, I am <span className="font-semibold">Nischay</span>👋🏻
+      Hi, I am <span className="font-semibold">aaa</span>👋🏻
       <br /> A Web and Mobile App Developer
     </h1>
   ),
