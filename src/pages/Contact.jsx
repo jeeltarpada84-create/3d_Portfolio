be3a66c091659ca9a1d45bab42f4b207015a0f58
jeeltarpada84-create <jeeltarpada84@gmail.com>
2026-07-04 20:31:24 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,7 +31,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Nischay",
+          to_name: "SSS",
           from_email: form.email,
           to_email: "nischay.patel.code@gmail.com",
           message: form.message,
